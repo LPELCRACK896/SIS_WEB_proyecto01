d33e86785368a7aaf8c4221f72b58a0c6415118a
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-28 23:52:44 -0600

    second section done

--- src/components/card/SimpleCard.js
+++ src/components/card/SimpleCard.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const SimpleCard = () => {
+const SimpleCard = (props) => {
   return (
-    <div>SimpleCard</div>
+    <div className='grid-mini-card'>
+      <div className='text-container-mini-card'>
+        <p className='title-mini-card'>
+          {props.title}
+        </p>
+        <p className='sub-title-mini-card'>
+          {props.subTitle}
+        </p>
+      </div>
+      <div className='image-side-mini-card' style={{ backgroundImage: `url(${props.backgroundImage})`}}>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/second-section/SecondSection.js
+++ src/components/second-section/SecondSection.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import MainBanner from '../../media/second_section/whm-riotnet-graphic.png'
+import SimpleCard from '../card/SimpleCard'
+import Raze from '../../media/second_section/raze-mini-card.png'
+import Rosa from '../../media/second_section/fortichexrg-4.png'
+import Black from '../../media/second_section/black.jpg'
+import Fist from '../../media/second_section/Fist.png'
 const SecondSection = () => {
   return (
     <div className='main-container-second-section'>
         <div className='first-flex-second-section'> 
-            <h1 >What's happening</h1>
+            <h1 >What's happening?</h1>
             <button className='see-more-button'>SEE MORE</button>
         </div>
         <div className='grid-container-second-section'>
             <div className='second-section-grid-main-card' style={{ backgroundImage: `url(${MainBanner})` }}/>
             <div className='second-section-grid-other-cards-space'>
-                <div className='mini-card-section-2'></div>
-                <div className='mini-card-section-2'></div>
-                <div className='mini-card-section-2'></div>
-                <div className='mini-card-section-2'></div>
+                <div className='mini-card-section-2'>
+                    <SimpleCard backgroundImage={Raze} title="How Rioters in Brazil Helped Create Raze, the Dancing Salvadorian..." subTitle="INSIDE RIOT"/>
+                </div>
+                <div className='mini-card-section-2'>
+                    <SimpleCard backgroundImage={Rosa} title="Riot Games Announces New Equity Investment in “Arcane” Animation..." subTitle="NEWS"/>
+                </div>
+                <div className='mini-card-section-2'>
+                    <SimpleCard backgroundImage={Black} title="Black History Month Recap" subTitle="INSIDE RIOT"/>
+                </div>
+                <div className='mini-card-section-2'>
+                    <SimpleCard backgroundImage={Fist} title="Riot is Donating Battle Pass Sales to Humanitarian Relief..." subTitle="NEWS"/>
+                </div>
 
             </div>
 
